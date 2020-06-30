5f46d32e28c7db641692111d39b8d180536c4fae
rinamukae <verbal5bule@icloud.com>
2020-06-30 22:59:28 +0900

    テストと辞書画面のプロトタイプを作成

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ var favicon = require('serve-favicon');
 var formRouter = require('./routes/form');
 var indexRouter = require('./routes/index');
 var resultRouter = require('./routes/result');
+var selectRouter = require('./routes/select');
+var aRouter = require('./routes/a');
 
 var app = express();
 
 //ファビコンの設定
 app.use(favicon(__dirname + '/public/image/favicon.ico'));
+
+//スタイルシート
+app.use(express.static(__dirname));
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -28,7 +33,8 @@ app.set('view engine', 'ejs');
 app.use('/form', formRouter);
 app.use('/', indexRouter);
 app.use('/result', resultRouter);
-
+app.use('/select', selectRouter);
+app.use('/a', aRouter);
 
 var server = http.createServer(app);
 var port = 8000;
--- routes/select.js
+++ routes/select.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get("/", function(req, res) {
 
-  res.render("index");
+  res.render("select");
 
 });
 
